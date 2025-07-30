50afcd99460352e002ab4b87b343f1f1435c9bdd
Prabesh Subedi <prabesh.sb@gmail.com>
2025-07-30 18:21:12 +0545

    properly initialize game state

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from bagchal import GameState
+from bagchal import GameState, Piece
 from game import Game
 if __name__ == "__main__":
-    board = [0] * 25
-    gameState = GameState(board, turn=-1 ,goat_count=20, eaten_goat_count=0)
+    board = [Piece.EMPTY] * 25
+    pos_tiger = [0, 4, 20, 24]
+    board[pos_tiger[0]] = Piece.TIGER
+    board[pos_tiger[1]] = Piece.TIGER
+    board[pos_tiger[2]] = Piece.TIGER
+    board[pos_tiger[3]] = Piece.TIGER
+
+    gameState = GameState(board, turn=Piece.GOAT,
+                          goat_count=20, eaten_goat_count=0)
     game = Game(game_state=gameState)
     game.run()
