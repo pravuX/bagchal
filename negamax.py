727a138f73eacac5d060831b080d4f23e8a7615c
Prabesh Subedi <prabesh.sb@gmail.com>
2025-10-16 15:37:31 +0545

    Add killers and history heuristic

--- negamax.py
+++ negamax.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from bagchal import *
 
 EXACT_FLAG, ALPHA_FLAG, BETA_FLAG = 0, 1, 2
@@ -59,6 +60,12 @@ class AlphaBetaAgent():
         self.game_state: BitboardGameState
         self.no_of_nodes = 0
 
+        # killer moves
+        # (ply, turn) -> killer1, killer2
+        self.killers = {}
+        # (square, turn) -> no of cutoffs
+        self.history = defaultdict(int)
+
     def get_best_move(self, gs, game_history=None, time_limit=1.5):
         self.game_state = gs.copy()
         self.game_history = game_history
@@ -83,8 +90,6 @@ class AlphaBetaAgent():
             return self.evaluate()
         if depth == 0:
             return self.evaluate()
-            # TODO: I'm not sure how useful qsearch is
-            # return self.qsearch(alpha, beta)
 
         old_alpha = alpha
 
@@ -93,9 +98,14 @@ class AlphaBetaAgent():
         # fallback in case the search doesnot find a best move
         best_move_so_far = moves[0]
 
-        for move in moves:
-            self.ply += 1
+        for i in range(len(moves)):
+
+            self.pick_move(moves, i)
+
+            move = moves[i]
+
             self.game_state.make_move(move)
+            self.ply += 1
 
             score = -self.negamax(-beta, -alpha, depth - 1)
 
@@ -104,16 +114,30 @@ class AlphaBetaAgent():
 
             # fail-hard beta cutoff
             if score >= beta:
+
+                if self.is_quiet(move):
+                    killer_key = (self.ply, self.game_state.turn)
+                    killers = self.killers.get(killer_key, [None, None])
+
+                    killers[1] = killers[0]
+                    killers[0] = move
+
+                    self.killers[killer_key] = killers
+
                 # node (move) fails high
                 if self.ply == 0:
                     # TODO: this is block is not executed at all why?
-                    # it means that there are no cutoffs happening at the root node
+                    # it means that there are no cutoffs happening at the root node!!
                     print("inshallah")
                     self.best_move = move
                 return beta
 
             # found a better move
             if score > alpha:
+
+                if self.is_quiet(move):
+                    history_key = (self.game_state.turn, move[1])
+                    self.history[history_key] += depth
                 # PV node (move)
                 if self.ply == 0:  # root
                     # associate best move with the best score
@@ -127,45 +151,52 @@ class AlphaBetaAgent():
         # node (move) fails low i.e. score <= alpha
         return alpha
 
-    def qsearch(self, alpha, beta):
-        self.no_of_nodes += 1
-
-        val = self.evaluate()
+    def is_quiet(self, move):
         if self.game_state.turn == Piece_GOAT:
-            return val
+            return True
+        src, dst = move
+        # if src and dst are adjacent, then the move is a non-capture
+        return MOVE_MASKS[src] & (1 << dst) != 0
 
-        if val >= beta:
-            # node (move) fails high
-            return beta
+    def pick_move(self, moves, current_idx):
+        best_score = float('-inf')
+        best_idx = current_idx
 
-        # found a better move
-        if val > alpha:
-            # PV node (move)
-            alpha = val
+        killer_key = (self.ply, self.game_state.turn)
+        killer1, killer2 = self.killers.get(killer_key, [None, None])
+        for j, move in enumerate(moves[current_idx:len(moves)]):
+            score = self._score_move(move)
+            # if move == hashed_move:
+            #     score += 1000
 
-        moves = self.game_state.get_legal_moves(only_captures=True)
+            if move == killer1:
+                score += 1000
+            elif move == killer2:
+                score += 900
+            else:
+                # move = (src, dst)
+                # TODO: i'm not sure, that unique moves will be chosen this way
+                history_key = (self.game_state.turn, move[1])
+                history_heuristic = self.history[history_key]
 
-        for move in moves:
-            self.ply += 1
-            self.game_state.make_move(move)
+                score += history_heuristic
 
-            score = -self.qsearch(-beta, -alpha)
+            if score > best_score:
+                best_score = score
+                best_idx = j + current_idx  # offset
 
-            self.game_state.unmake_move()
-            self.ply -= 1
+        moves[current_idx], moves[best_idx] = moves[best_idx], moves[current_idx]
 
-            # fail-hard beta cutoff
-            if score >= beta:
-                # node (move) fails high
-                return beta
+    def _score_move(self, move):
+        # TODO: why does static move sorting result in slightly higher no of nodes searched?
+        if self.game_state.turn == Piece_TIGER:
+            return tiger_priority(self.game_state.tigers_bb, self.game_state.goats_bb, move, MOVE_MASKS_NP, CAPTURE_COUNTS, CAPTURE_MASKS_NP)
+        else:
+            return goat_priority(self.game_state.tigers_bb, self.game_state.goats_bb, move, MOVE_MASKS_NP, CAPTURE_COUNTS, CAPTURE_MASKS_NP, OUTER_EDGE_MASK, STRATEGIC_MASK)
 
-            # found a better move
-            if score > alpha:
-                # PV node (move)
-                alpha = score
-
-        # node (move) fails low i.e. score <= alpha
-        return alpha
+        # if self.game_state.turn == Piece_TIGER:
+        #     return 0.0
+        # return goat_priority(self.game_state.tigers_bb, self.game_state.goats_bb, move, MOVE_MASKS_NP, CAPTURE_COUNTS, CAPTURE_MASKS_NP, OUTER_EDGE_MASK, STRATEGIC_MASK)
 
     def evaluate(self):
         """
@@ -173,9 +204,6 @@ class AlphaBetaAgent():
         """
 
         state = self.game_state
-
-        occupied_bb = state.tigers_bb | state.goats_bb
-        empty_bb = ~occupied_bb & BOARD_MASK
 
         is_placement = state.goats_to_place > 0
 
@@ -203,13 +231,13 @@ class AlphaBetaAgent():
 
         eaten_score = eaten / eat_max
 
-        potential_captures = self._count_potential_captures(state, empty_bb)
+        potential_captures = self._count_potential_captures()
         potential_capture_score = potential_captures / potential_capture_max
         potential_capture_score = min(1, potential_capture_score)
 
         trap_score = trapped / trap_max
         goat_presence_score = goats_on_board / goat_presence_max
-        accessible, inaccessible = self._get_tiger_accessibility(state)
+        accessible, inaccessible = self._get_tiger_accessibility()
 
         inaccessibility_score = inaccessible / inaccessible_max
         tiger_mobility_score = accessible / tiger_mobility_max
@@ -231,26 +259,22 @@ class AlphaBetaAgent():
         final_evaluation = tiger_score - goat_score
         return final_evaluation * state.turn
 
-    def _count_potential_captures(self, state: BitboardGameState, empty_bb):
+    def _count_potential_captures(self):
+
+        occupied_bb = self.game_state.tigers_bb | self.game_state.goats_bb
+        empty_bb = ~occupied_bb & BOARD_MASK
 
         capture_opportunities = 0
-        # just in case, if i decide to "JIT" this with numba
-        # for tiger in extract_indices_fast(state.tigers_bb):
-        #     for j in range(CAPTURE_COUNTS[tiger]):
-        #         mid_mask = CAPTURE_MASKS_NP[tiger, j, 0]
-        #         land_mask = CAPTURE_MASKS_NP[tiger, j, 1]
-        #         if (state.goats_bb & mid_mask) and (empty_bb & land_mask):
-        #             capture_opportunities += 1
 
-        for tiger in extract_indices_fast(state.tigers_bb):
+        for tiger in extract_indices_fast(self.game_state.tigers_bb):
             for mid_mask, land_mask in CAPTURE_MASKS[tiger]:
-                if (state.goats_bb & mid_mask) and (empty_bb & land_mask):
+                if (self.game_state.goats_bb & mid_mask) and (empty_bb & land_mask):
                     capture_opportunities += 1
 
         return capture_opportunities
 
-    def _get_tiger_accessibility(self, state: BitboardGameState):
+    def _get_tiger_accessibility(self):
         accessible, inaccessible = tiger_board_accessibility(
-            state.tigers_bb, state.goats_bb,
+            self.game_state.tigers_bb, self.game_state.goats_bb,
             MOVE_MASKS_NP, CAPTURE_COUNTS, CAPTURE_MASKS_NP)
         return accessible, inaccessible
